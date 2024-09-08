9f5ebeea89d93faf85e40e9717987acb0d500ec1
Trey Shneour <tshneour@ucsd.edu>
2024-09-07 23:42:34 -0700

    Initial commit

---
+++ AA_alignment.py
@@ -0,0 +1,198 @@
+from Bio import Align
+from Bio.Seq import Seq
+from Bio.Align import substitution_matrices
+import pandas as pd
+import subprocess
+import re
+
+def check_features(ori):
+    if ori == "++":
+        return "foldback"
+    elif ori == "--":
+        return "inversion"
+    elif ori == "+-":
+        return "deletion-like"
+    elif ori == "-+":
+        return "duplication-like"
+    else:
+        return "Unknown"
+
+def extract_region(fasta_file, region):
+    """
+    Extracts a specific region from a FASTA file using samtools.
+
+    :param fasta_file: Path to the indexed FASTA file.
+    :param region: Region in the format "chr:start-end" (e.g., "chr18:180-280").
+    :return: The sequence as a string.
+    """
+    result = subprocess.run(
+        ["samtools", "faidx", fasta_file, region],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+    if result.returncode == 0:
+        # The sequence will be in the second line, as the first line contains the header
+        return ''.join(result.stdout.splitlines()[1:])
+    else:
+        raise Exception(f"Error: {result.stderr.strip()}")
+
+def reverse_complement(string):
+    seq = Seq(string)
+    rc = seq.reverse_complement()
+    return str(rc)
+
+def extract_affix(cigar):
+    # Pattern 1: (num1)S(num2)M
+    pattern1 = re.compile(r'^(\d+)[SH]')
+    # Pattern 2: (num1)M(num2)S
+    pattern2 = re.compile(r'(\d+)[SH]$')
+
+    # Check for first pattern (num1)S(num2)M
+    match1 = pattern1.search(cigar)
+    if match1:
+        return False, match1.group(1)  # Return num1 (the number followed by S)
+    
+    # Check for second pattern (num1)M(num2)S
+    match2 = pattern2.search(cigar)
+    if match2:
+        return True, match2.group(1)  # Return num2 (the number followed by S)
+    
+    # If no pattern is matched
+    raise ValueError("Cigar string doesn't match (num1)S(num2)M or (num1)M(num2)S\n", cigar)
+
+if __name__ == "__main__":
+    fasta_file = "./hg19/hg19full.fa"
+    df = pd.read_csv("./alignments.tsv", sep="\t")
+    # aligner = Align.PairwiseAligner(mode="global", open_gap_score = -10, extend_gap_score = -0.5, match_score = 1.0, mismatch_score = -1.0)
+    aligner = Align.PairwiseAligner(mode="local", open_gap_score = -10, extend_gap_score = -5, match_score=2, mismatch_score=-2)
+
+    it = iter(df.iterrows())
+    for index, row in it:
+        homology = row["break_homology_seq"]
+        if row["split"] == False or pd.isna(homology):
+            continue
+        
+        # Collect AA breakpoint details
+        break_chr1 = row["break_chrom1"]
+        break_pos1 = row["break_pos1"]
+        break_chr2 = row["break_chrom2"]
+        break_pos2 = row["break_pos2"]
+        break_ori = row["break_orientation"]
+        break_sv_type = row["break_sv_type"]
+        break_read_support = row["break_read_support"]
+        break_features = row["break_features"]
+        break_hom_len = row["break_homology_len"]
+
+        query_name = row["query_name"]
+        query_is_proper = row["proper_pair"]
+        query_read_num = row["read_num"]
+
+        second_row = next(it)[1]
+
+        if 'H' in row["query_cigar"]:
+            temp = row
+            row = second_row
+            second_row = temp
+
+        first_chr = row["query_chrom"]
+        first_ori = row["query_orientation"]
+        # first_aln = Seq(row["query_aln_sub"]) if row["orientation"][0]=="+" else Seq(row["query_aln_sub"]).reverse_complement()
+        first_aln = row["query_aln_sub"]
+        first_cigar = row["query_cigar"]
+        first_pos = row["query_pos"]
+        first_end = row["query_end"]
+        first_len = len(first_aln)
+
+        second_chr = second_row["query_chrom"]
+        second_ori = second_row["query_orientation"]
+        # second_aln = Seq(second_row["query_aln_sub"]) if row["orientation"][1]=="+" else Seq(second_row["query_aln_sub"]).reverse_complement()
+        second_aln = second_row["query_aln_sub"]
+        second_cigar = second_row["query_cigar"]
+        second_pos = second_row["query_pos"]
+        second_end = second_row["query_end"]
+        second_len = len(second_aln)
+
+        primary_aln = row["query_aln_full"]
+        primary_len = len(primary_aln)
+
+        #Get orientation and number of clipped bases from both cigars
+        pr_ori, pr_affix = extract_affix(first_cigar)
+        sup_ori, sup_affix = extract_affix(second_cigar)
+        pr_affix = int(pr_affix)
+        sup_affix = int(sup_affix)
+        hom_len = None
+        hom_seq = None
+
+        # Calculate homology length
+        if pr_ori:
+                sup_aln = reverse_complement(second_aln) if sup_ori else second_aln
+                sup_aln_len = len(sup_aln)
+                suffix_len = pr_affix
+                hom_len = (primary_len - suffix_len) - (primary_len - sup_aln_len)
+                if hom_len > 0:
+                    hom_seq = primary_aln[primary_len - sup_aln_len: primary_len - suffix_len]
+                elif hom_len < 0:
+                    hom_seq = primary_aln[primary_len - suffix_len: primary_len - sup_aln_len]
+        else:
+            sup_aln = second_aln if sup_ori else reverse_complement(second_aln)
+            sup_aln_len = len(sup_aln)
+            prefix_len = pr_affix
+            hom_len = sup_aln_len - prefix_len
+            if hom_len > 0:
+                hom_seq = primary_aln[prefix_len: sup_aln_len]
+            elif hom_len < 0:
+                hom_seq = primary_aln[sup_aln_len: prefix_len]
+
+        # Extract reference sequences such that both donor sequences meet in the middle
+        region1 = f"{first_chr}:{first_pos - (100 - first_len)}-{first_pos+first_len+9}" if pr_ori else f"{first_chr}:{first_pos - 10}-{first_pos+99}"
+        sequence1 = extract_region(fasta_file, region1)
+        region2 = f"{second_chr}:{second_pos-10}-{second_pos+99}" if ((not pr_ori and not sup_ori) or (pr_ori and not sup_ori)) else f"{second_chr}:{second_pos - (100 - second_len)}-{second_pos+second_len+9}"
+        sequence2 = extract_region(fasta_file, region2)
+        
+        # Reverse complement supplementary alignment if necessary to align with primary alignment
+        second_aln = reverse_complement(second_aln) if (pr_ori and sup_ori) or (not pr_ori and not sup_ori) else second_aln
+        sequence2 = reverse_complement(sequence2) if (pr_ori and sup_ori) or (not pr_ori and not sup_ori) else sequence2
+        full_reference = (sequence1 + sequence2).upper() if pr_ori else (sequence2 + sequence1).upper()
+        
+        print("Fetched split read near AA breakpoint. Displaying breakpoint details:")
+        print(row[0:4].T.to_string(header=False))
+        print(row[4:10].T.to_string(header=False), "\n")
+
+        print("Split read details:")
+        print(row[10:11].T.to_string(header=False))
+        print("Primary Alignment" if 'S' in first_cigar else "Supplementary Alignment")
+        print(row[13:20].T.to_string(header=False))
+        print("length", 3*"\t", first_len)
+        print("Raw Sequence", 2*"\t", row["query_aln_sub"], "\n")
+        print(second_row[10:11].T.to_string(header=False))
+        print("Primary Alignment" if 'S' in second_cigar else "Supplementary Alignment")
+        print(second_row[13:20].T.to_string(header=False))
+        print("length", 3*"\t", second_len)
+        print("Raw Sequence", 2*"\t", second_row["query_aln_sub"], "\n")
+
+        print("Split Read Alignment Visualization:")
+        print("Primary first then Sup" if pr_ori else "Sup first then Primary")
+        alignments1 = aligner.align(full_reference, first_aln if pr_ori else second_aln)
+        alignments2 = aligner.align(full_reference, first_aln if not pr_ori else second_aln)
+        for aln in alignments1:
+            print(aln)
+        if hom_len < 0:
+            print("Gap detected between donor sequences:", hom_seq)
+        for aln in alignments2:
+            print(aln)
+        
+        calc_ori = ("+" if pr_ori else "-") + ("+" if sup_ori else "-")
+        print("Calculated Breakpoint")
+        print("chr1", 2*"\t", first_chr)
+        print("pos1", 2*"\t", first_pos if calc_ori[0] == "-" else first_end)
+        print("chr2", 2*"\t", second_chr)
+        print("pos2", 2*"\t", second_pos if calc_ori[1] == "-" else second_end)
+        print("orientation", "\t", calc_ori)
+        print("Orientation is based on above order of breakpoint ends!")
+        print("features", "\t", check_features(calc_ori))
+        print("homology_length", "\t", hom_len)
+        print("homology_sequence", "\t", hom_seq)
+
+        print("\n")
+
---
+++ alignment.py
@@ -0,0 +1,232 @@
+import pandas as pd
+import pysam
+import re
+import argparse
+
+def get_pairs(regions, chrom1, chrom2):
+    """
+    Fetch all paired reads, including those with split alignments
+
+    Parameters:
+    - regions: the two SV regions to collect reads from
+    - chrom1: chromosome # of first region
+    - chrom2: chromosome # of second region
+
+    Returns:
+    - split_alignments: contains all sets of paired reads with split alignments
+    - paired_alignments: contains all sets of paired reads without split alignments
+    """
+    split_alignments = []
+    nonsplit_alignments = []
+
+    # Iterate over first breakpoint end
+    for i, region in enumerate(regions):
+        for aln in region:
+            # If we've seen this read before, skip it
+            if any(aln in tup for tup in split_alignments) or any(aln in tup for tup in nonsplit_alignments):
+                print("Duplicate found")
+                continue
+
+            mate = samfile.mate(aln)
+
+            if aln.is_supplementary or aln.has_tag("SA"):
+                sa_tag = (aln.get_tag("SA")).split(";")[:-1]
+                sa_tag = sa_tag[0]
+                sup_name, sup_pos, sup_strand, sup_cigar, sup_mapq, sup_tlen = sa_tag.split(",")
+                sup_pos = int(sup_pos)
+                sup_tlen = int(sup_tlen)
+
+                found_split2 = False
+                for sup_read in samfile.fetch(sup_name, sup_pos, sup_pos + 1):
+                    if (sup_read.query_name == aln.query_name and sup_read.reference_name == sup_name and (sup_read.reference_start + 1) == sup_pos and re.sub(r'[SH]', '', sup_read.cigarstring) == re.sub(r'[SH]', '', sup_cigar)):
+                        non_sup = aln if not aln.is_supplementary else sup_read
+                        sup = aln if aln.is_supplementary else sup_read
+                        read1 = non_sup if non_sup.is_read1 else mate
+                        read2 = non_sup if non_sup.is_read2 else mate
+                        split_alignments.append((read1, sup, read2))
+                        found_split2 = True
+                        break
+                if not found_split2:
+                    print(sup_name, sup_pos, sup_cigar, sup_tlen)
+                    raise NameError("split mate not found")
+
+            elif mate.is_supplementary or mate.has_tag("SA"):
+                sa_tag = (mate.get_tag("SA")).split(";")[:-1]
+                sa_tag = sa_tag[0]
+                sup_name, sup_pos, sup_strand, sup_cigar, sup_mapq, sup_tlen = sa_tag.split(",")
+                sup_pos = int(sup_pos)
+                sup_tlen = int(sup_tlen)
+
+                found_split2 = False
+                for sup_read in samfile.fetch(sup_name, sup_pos, sup_pos + 1):
+                    if (sup_read.query_name == mate.query_name and sup_read.reference_name == sup_name and (sup_read.reference_start + 1) == sup_pos and re.sub(r'[SH]', '', sup_read.cigarstring) == re.sub(r'[SH]', '', sup_cigar)):
+                        non_sup = mate if not mate.is_supplementary else sup_read
+                        sup = mate if mate.is_supplementary else sup_read
+                        read1 = non_sup if non_sup.is_read1 else aln
+                        read2 = non_sup if non_sup.is_read2 else aln
+                        split_alignments.append((read1, sup, read2))
+                        found_split2 = True
+                        break
+                if not found_split2:
+                    print(sup_name, sup_pos, sup_cigar, sup_tlen)
+                    raise NameError("split mate not found")
+                
+            elif not aln.is_proper_pair and aln.next_reference_name == (chrom2 if i == 0 else chrom1):
+                read1 = aln if aln.is_read1 else mate
+                read2 = aln if aln.is_read2 else mate
+                nonsplit_alignments.append((read1, read2))
+            
+            elif aln.is_proper_pair:
+                read1 = aln if aln.is_read1 else mate
+                read2 = aln if aln.is_read2 else mate
+                nonsplit_alignments.append((read1, read2))
+    
+    return split_alignments, nonsplit_alignments
+
+def fetch_alignments(bamfile, chrom1, pos1, chrom2, pos2, sv_type, read_support, features, orientation, hom_len, hom, region_size=150, mapq_threshold=15):
+    """
+    Fetch reads from a BAM file that have split alignments 
+    between two genomic coordinates as well as all paired-end reads.
+
+    Parameters:
+    - bamfile: str, path to the BAM file.
+    - chrom1: str, chromosome for the first position.
+    - pos1: int, first position in the genome.
+    - chrom2: str, chromosome for the second position.
+    - pos2: int, second position in the genome.
+    - region_size: int, the size of the region around the positions to fetch alignments from.
+    - mapq_threshold: int, cutoff for collecting reads above certain mapping quality.
+
+    Returns:
+    - split_df: DataFrame, details of split alignments.
+    - paired_df: DataFrame, details of discordant alignments.
+    """
+    # Open the BAM file
+    samfile = pysam.AlignmentFile(bamfile, "rb")
+
+    # Fetch alignments region_size away from both positions
+    region1 = samfile.fetch(chrom1, pos1 - region_size, pos1 + region_size + 1)
+    region2 = samfile.fetch(chrom2, pos2 - region_size, pos2 + region_size + 1)
+
+    split_alignments, nonsplit_alignments = get_pairs([region1, region2], chrom1, chrom2)
+
+    # Collect details of split alignments
+    split_alignment_details = []
+    for pair in split_alignments:
+        read1_1, read1_2, read2 = pair
+
+        # Filter out low quality reads
+        if  ((read1_1.mapping_quality > mapq_threshold and read1_2.mapping_quality > mapq_threshold) or read2.mapping_quality > mapq_threshold) or ((read1_1.is_mapped and read1_2.is_mapped) or read2.is_mapped):
+            for read in pair:
+                split_alignment_details.append({
+                    "break_chrom1": chrom1,
+                    "break_pos1": pos1,
+                    "break_chrom2": chrom2,
+                    "break_pos2": pos2,
+                    "break_sv_type": sv_type,
+                    "break_read_support": read_support,
+                    "break_features": features,
+                    "break_orientation": orientation,
+                    "break_homology_len": hom_len,
+                    "break_homology_seq": hom,
+                    "query_name": read.query_name,
+                    "query_short": read.query_name.split(':')[-1],
+                    "split": read.has_tag("SA"),
+                    "proper_pair": "Concordant" if read.is_proper_pair else "Discordant",
+                    "read_num": "1" if read.is_read1 else "2",
+                    "query_chrom": read.reference_name,
+                    "query_pos": read.reference_start + 1,
+                    "query_end": read.reference_end + 1,
+                    "query_orientation": "+" if read.is_forward else "-",
+                    "query_cigar": read.cigarstring,
+                    "query_aln_full": read.query_sequence,
+                    "query_aln_sub": read.query_alignment_sequence
+                })
+
+    # Collect details of nonsplit alignments
+    nonsplit_alignment_details = []
+    for pair in nonsplit_alignments:
+        read1, read2 = pair
+        # Filter out low quality reads
+        if  (read1.mapping_quality > mapq_threshold or read2.mapping_quality > mapq_threshold) or (read1.is_mapped or read2.is_mapped):
+            for read in pair:
+                nonsplit_alignment_details.append({
+                    "break_chrom1": chrom1,
+                    "break_pos1": pos1,
+                    "break_chrom2": chrom2,
+                    "break_pos2": pos2,
+                    "break_sv_type": sv_type,
+                    "break_read_support": read_support,
+                    "break_features": features,
+                    "break_orientation": orientation,
+                    "break_homology_len": hom_len,
+                    "break_homology_seq": hom,
+                    "query_name": read.query_name,
+                    "query_short": read.query_name.split(':')[-1],
+                    "split": read.has_tag("SA"),
+                    "proper_pair": "Concordant" if read1.is_proper_pair else "Discordant",
+                    "read_num": "1" if read.is_read1 else "2",
+                    "query_chrom": read.reference_name,
+                    "query_pos": read.reference_start + 1,
+                    "query_end": read.reference_end + 1,
+                    "query_orientation": "+" if read.is_forward else "-",
+                    "query_cigar": read.cigarstring,
+                    "query_aln_full": read.query_sequence,
+                    "query_aln_sub": read.query_alignment_sequence
+                })
+        
+    samfile.close()
+
+    # Convert details to pandas DataFrames
+    split_df = pd.DataFrame(split_alignment_details)
+    nonsplit_df = pd.DataFrame(nonsplit_alignment_details)
+    # paired_df = pd.DataFrame(paired_alignment_details)
+
+    return split_df, nonsplit_df
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Collect reads from the ends of a SV.")
+    parser.add_argument("--summaries", help="path to AA amplicon summaries file", type=str, default="./K562/K562_summaries.tsv")
+    parser.add_argument("--bam", help="path to bamfile", type=str, default="./K562/K562_hg19_cnvkit.cs.rmdup.bam")
+    args = parser.parse_args()
+    # Define the BAM file and positions
+    bamfile = args.bam
+    samfile = pysam.AlignmentFile(bamfile, "rb")
+    df = pd.read_csv(args.summaries, sep="\t")
+    # Define output table
+    output = pd.DataFrame()
+    num_split = 0
+    num_discord = 0
+    num_concord = 0
+    num_paired = 0
+
+    for index, row in df.iterrows():
+        chrom1 = row["chrom1"]
+        pos1 = row["pos1"]
+        chrom2 = row["chrom2"]
+        pos2 = row["pos2"]
+        sv_type = row["sv_type"]
+        read_support = row["read_support"]
+        features = row["features"]
+        orientation = row["orientation"]
+        hom_len = row["homology_length"]
+        homology = row["homology_sequence"]
+
+        # Fetch the DataFrames for split alignments and paired alignments
+        split_df, nonsplit_df = fetch_alignments(bamfile, chrom1, pos1, chrom2, pos2, sv_type, read_support, features, orientation, hom_len, homology, 150)
+        num_split += len(split_df)/3
+        num_paired += (2 * len(split_df) / 3) + len(nonsplit_df)
+        # num_paired += len(paired_df)
+        
+
+        print(f"Reads at junction defined by {chrom1} {pos1} and {chrom2} {pos2}:")
+        print(f"Number split pairs: {len(split_df)/3}")
+        print(f"Number nonsplit pairs: {len(nonsplit_df)/2}")
+        print(f"Number paired: {(2 * len(split_df) / 3) + len(nonsplit_df)}")
+        output = pd.concat([output, split_df], ignore_index=True)
+        output = pd.concat([output, nonsplit_df], ignore_index=True)
+
+    output.to_csv('alignments.tsv', sep='\t', index=False)
+
+    print("Total Number of split reads:", num_split)
+    print("Total Number of paired reads:", num_paired)
